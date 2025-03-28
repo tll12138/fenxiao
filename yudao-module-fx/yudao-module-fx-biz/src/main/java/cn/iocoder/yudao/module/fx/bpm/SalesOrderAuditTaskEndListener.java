cd167e0eb56208f678b78809f6c60bf814254174
tanglilong <tanglilong@puqi.group>
2025-03-28 11:04:04 +0800

    销售单审批流----优化销售单审核归档节点动作

--- yudao-module-fx/yudao-module-fx-biz/src/main/java/cn/iocoder/yudao/module/fx/bpm/SalesOrderAuditTaskEndListener.java
+++ yudao-module-fx/yudao-module-fx-biz/src/main/java/cn/iocoder/yudao/module/fx/bpm/SalesOrderAuditTaskEndListener.java
@@ -19,11 +19,33 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Set;
+import java.util.StringJoiner;
 
 @Component
 @Slf4j
 public class SalesOrderAuditTaskEndListener {
+
+    // 常量定义
+    private static final Set<String> SPECIAL_BRANDS =
+            Collections.unmodifiableSet(new HashSet<>(Arrays.asList("FREIOL", "E-CHANGE", "VALGER")));
+    private static final Set<String> YIWU_WAREHOUSES =
+            Collections.unmodifiableSet(new HashSet<>(Arrays.asList("11733495", "10790722", "10860196", "11353580")));
+    private static final String FULAI_WAREHOUSE = "12367046";
+    private static final String WUHU_WAREHOUSE = "11717238";
+    private static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private static final String LOG_MODULE = "fx";
+    private static final String LOG_TYPE = "SalesOrderAuditTaskEndListener";
+    private static final String DD_WEBHOOK = "https://oapi.dingtalk.com/robot/send?access_token=66a1668bb391683c4855e1bf6658eafbf39b62850e59e67eef2ab4cbae2377f2";
+    private static final String DD_SECRET = "SEC155b0da885add21530ee3602a66309ab7e054e0047889551950ade24176b3383";
+    private static final String WUH_WEBHOOK = "http://10.10.4.30:5000/send_wechat";
+    private static final String MESSAGE_SEPARATOR = "|";
+    private static final String PP_WAREHOUSE = "11353580";
+    private static final String BOX_SHIPMENT = "按箱规发货";
+    private static final String PRODUCT_PROTECTION = "做好产品防护!";
+    private static final String TRACEABILITY_CODE = "需要扫溯源码出库";
 
     /**
      * 销售单审核归档节点动作
@@ -31,95 +53,128 @@ public class SalesOrderAuditTaskEndListener {
     @Transactional(rollbackFor = Exception.class)
     public void execute(DelegateExecution delegateExecution) {
         String errorMsg = StrUtil.EMPTY;
+        final String processId = delegateExecution.getProcessInstanceId();
         // 通过流程实例ID关联业务数据
-        final String processInstanceId = delegateExecution.getProcessInstanceId();
         try {
             //flowable流处理中不能用依赖注入，只能用SpringUtil.getObject获取bean
             OrdersInfoService ordersInfoService = SpringUtil.getObject(OrdersInfoService.class);
-            OrdersInfoDetailRespVO ordersInfo = validateOrder(processInstanceId, ordersInfoService);
+            OrdersInfoDetailRespVO ordersInfo = validateOrder(processId, ordersInfoService);
             CustomerInfoService customerInfoService = SpringUtil.getObject(CustomerInfoService.class);
             CustomerInfoDO customerInfo = customerInfoService.getCustomerInfo(ordersInfo.getDistributorId());
             //初始化延迟时间 fl\ec\vg 特殊
             String brand = ordersInfo.getBrand();
-            final int delayMinutes = new HashSet<>(Arrays.asList("FREIOL", "E-CHANGE", "VALGER")).contains(brand) ? 3 : 8;
-            String lateTime = DateUtil.format(DateUtil.offsetMinute(DateUtil.date(), delayMinutes), "yyyy-MM-dd HH:mm:ss");
-            String warehouseCode = ordersInfo.getWarehouseCode();
-            String requirement = ordersInfo.getRequirement();
-            String orderId = ordersInfo.getOrderId();
-            String msg = StrUtil.EMPTY;
+            final int delayMinutes = SPECIAL_BRANDS.contains(brand) ? 3 : 8;
+            String lateTime = DateUtil.format(DateUtil.offsetMinute(DateUtil.date(), delayMinutes), DATE_PATTERN);
             SentMessageService sentMessageService = SpringUtil.getObject(SentMessageService.class);
-            final String DD_webhook = "https://oapi.dingtalk.com/robot/send?access_token=66a1668bb391683c4855e1bf6658eafbf39b62850e59e67eef2ab4cbae2377f2";
-            final String DD_secret = "SEC155b0da885add21530ee3602a66309ab7e054e0047889551950ade24176b3383";
-            if ("12367046".equals(warehouseCode)) {
-                //菜鸟义乌仓-福来
-                //效期要求
-                boolean validityPeriodRequire = StrUtil.contains(requirement, "年") || StrUtil.contains(requirement, "最新");
-                msg = validityPeriodRequire ? StrUtil.EMPTY : "按效期先后发货";
-                //商品中是否存在小样
-                Boolean checkSample = ordersInfoService.checkSample(ordersInfo.getId());
-                msg = checkSample ? "纯小样" + "|" + msg : "需要扫溯源码出库" + "|" + msg;
-                //箱规
-                Boolean checkBoxSize = ordersInfoService.checkBoxSize(ordersInfo.getId());
-                StringBuilder append = StrUtil.builder().append("|").append(requirement);
-                msg = checkBoxSize ? append.append("|按箱规发货。").toString() : append.toString();
-                sentMessageService.createSentMessage(new SentMessageSaveReqVO()
-                        .setType("发货要求")
-                        .setSendTime(LocalDateTime.parse(lateTime))
-                        .setWebhook(DD_webhook)
-                        .setSecret(DD_secret)
-                        .setWarehouseId(Integer.valueOf(warehouseCode))
-                        .setMsg("【freiol-2B出货要求】：" + "          " + "交易单号：" + orderId + "          " + "客商名称：" + customerInfo.getDisplayName() + "          " + "客商编码：" + customerInfo.getDistributorNum() + "          " + "发货要求：" + msg));
-            } else if (new HashSet<>(Arrays.asList("11733495", "10790722", "10860196", "11353580")).contains(warehouseCode)) {
-                //菜鸟义乌仓-EC\VALGER\GD\企鹅
-                requirement = StrUtil.replace(requirement, "需要扫溯源码出库", StrUtil.EMPTY);
-                if (requirement.length() > 2) {
-                    msg = requirement;
-                }
-                if ("11353580".equals(warehouseCode)) {
-                    //pp需要扫溯源码
-                    msg = msg + "|需要扫溯源码出库|按箱规发货|做好产品防护!";
-                } else {
-                    msg = msg + "|按箱规发货|做好产品防护!";
-                }
-                sentMessageService.createSentMessage(new SentMessageSaveReqVO()
-                        .setType("发货要求")
-                        .setSendTime(LocalDateTime.parse(lateTime))
-                        .setWebhook(DD_webhook)
-                        .setSecret(DD_secret)
-                        .setWarehouseId(Integer.valueOf(warehouseCode))
-                        .setMsg("【" + brand + "-2B出货要求】：" + "          " + "交易单号：" + orderId + "          " + "客商名称：" + customerInfo.getDisplayName() + "          " + "客商编码：" + customerInfo.getDistributorNum() + "          " + "发货要求：" + msg));
-
-            } else if ("11717238".equals(warehouseCode)) {
-                //芜湖速发云仓
-                requirement = StrUtil.replace(requirement, "需要扫溯源码出库", StrUtil.EMPTY);
-                if (requirement.length() > 2) {
-                    msg = requirement;
-                }
-                msg = msg + "|按箱规发货|做好产品防护!";
-                sentMessageService.createSentMessage(new SentMessageSaveReqVO()
-                        .setType("发货要求")
-                        .setSendTime(LocalDateTime.parse(lateTime))
-                        .setWebhook("http://10.10.4.30:5000/send_wechat")
-                        .setSecret(StrUtil.EMPTY)
-                        .setWarehouseId(Integer.valueOf(warehouseCode))
-                        .setMsg("【" + brand + "-2B出货要求】：" + "          " + "交易单号：" + orderId + "          " + "客商名称：" + customerInfo.getDisplayName() + "          " + "客商编码：" + customerInfo.getDistributorNum() + "          " + "发货要求：" + msg));
-            }
-
+            processWarehouse(ordersInfo.getWarehouseCode(), ordersInfo.getRequirement(), ordersInfoService, ordersInfo, sentMessageService
+                    , lateTime, ordersInfo.getOrderId(), customerInfo, brand);
         } catch (BusinessException e) {
-            errorMsg = e.getMessage();
-            log.error("[流程{}] 业务异常: {}", processInstanceId, errorMsg);
-            throw e; // 触发事务回滚
+            errorMsg = handleBusinessError(processId, e);
+            throw e;
         } catch (Exception e) {
-            errorMsg = StrUtil.format("系统异常: {}", e.getMessage());
-            log.error("[流程{}] 处理异常: ", processInstanceId, e);
-            throw new BusinessException(errorMsg);
+            errorMsg = handleSystemError(processId, e);
         } finally {
-            if (StrUtil.isNotBlank(errorMsg)) {
-                // 统一记录业务错误日志
-                BizErrorLogService bizErrorLogService = SpringUtil.getObject(BizErrorLogService.class);
-                bizErrorLogService.createBizErrorLog("fx", "SalesOrderAuditTaskEndListener", processInstanceId, null, errorMsg);
-            }
+            logErrorIfNeeded(processId, errorMsg);
         }
+    }
+
+    /**
+     * 处理不同仓库的发货要求
+     *
+     * @param warehouseCode      仓库编码
+     * @param requirement        发货要求
+     * @param ordersInfoService  订单服务
+     * @param ordersInfo         订单信息
+     * @param sentMessageService 消息服务
+     * @param lateTime           延迟发送时间
+     * @param orderId            订单ID
+     * @param customerInfo       客户信息
+     * @param brand              品牌
+     */
+    private static void processWarehouse(String warehouseCode, String requirement, OrdersInfoService ordersInfoService, OrdersInfoDetailRespVO ordersInfo, SentMessageService sentMessageService, String lateTime, String orderId, CustomerInfoDO customerInfo, String brand) {
+
+        SentMessageSaveReqVO message = buildBaseMessage(lateTime, warehouseCode, orderId, customerInfo, brand);
+        if (FULAI_WAREHOUSE.equals(warehouseCode)) {
+            handleFuLaiWarehouse(requirement, ordersInfoService, ordersInfo, message);
+        } else if (YIWU_WAREHOUSES.contains(warehouseCode)) {
+            handleYiWuWarehouse(warehouseCode, requirement, message);
+        } else if (WUHU_WAREHOUSE.equals(warehouseCode)) {
+            handleWuHuWarehouse(requirement, message);
+
+        }
+        sentMessageService.createSentMessage(message);
+    }
+
+    private static void handleFuLaiWarehouse(String requirement, OrdersInfoService ordersInfoService, OrdersInfoDetailRespVO ordersInfo, SentMessageSaveReqVO message) {
+        //菜鸟义乌仓-福来
+        boolean validityPeriodRequire = StrUtil.containsAny(requirement, "年", "最新");
+        Boolean checkSample = ordersInfoService.checkSample(ordersInfo.getId());
+        Boolean checkBoxSize = ordersInfoService.checkBoxSize(ordersInfo.getId());
+
+        String msg = buildFuLaiMessage(validityPeriodRequire, checkSample, checkBoxSize, requirement);
+        message.setMsg(msg)
+                .setWebhook(DD_WEBHOOK)
+                .setSecret(DD_SECRET);
+    }
+
+    private static void handleYiWuWarehouse(String warehouseCode, String requirement, SentMessageSaveReqVO message) {
+        //菜鸟义乌仓-EC\VALGER\GD\企鹅
+        String msg = StrUtil.EMPTY;
+        requirement = StrUtil.replace(requirement, TRACEABILITY_CODE, StrUtil.EMPTY);
+
+        if (requirement.length() > 2) {
+            msg = requirement;
+        }
+
+        StringJoiner joiner = new StringJoiner(MESSAGE_SEPARATOR);
+        if (PP_WAREHOUSE.equals(warehouseCode)) {
+            joiner.add(TRACEABILITY_CODE);
+        }
+        joiner.add(BOX_SHIPMENT).add(PRODUCT_PROTECTION);
+
+        message.setMsg(msg + MESSAGE_SEPARATOR + joiner)
+                .setWebhook(DD_WEBHOOK)
+                .setSecret(DD_SECRET);
+    }
+
+    private static void handleWuHuWarehouse(String requirement, SentMessageSaveReqVO message) {
+        //芜湖速发云仓
+        String msg = StrUtil.EMPTY;
+        requirement = StrUtil.replace(requirement, TRACEABILITY_CODE, StrUtil.EMPTY);
+
+        if (requirement.length() > 2) {
+            msg = requirement;
+        }
+
+        message.setMsg(msg + MESSAGE_SEPARATOR + BOX_SHIPMENT + MESSAGE_SEPARATOR + PRODUCT_PROTECTION)
+                .setWebhook(WUH_WEBHOOK)
+                .setSecret(StrUtil.EMPTY);
+    }
+
+    // 公共消息构建方法
+    private static SentMessageSaveReqVO buildBaseMessage(String lateTime, String warehouseCode,
+                                                         String orderId, CustomerInfoDO customerInfo, String brand) {
+
+        return new SentMessageSaveReqVO()
+                .setType("发货要求")
+                .setSendTime(LocalDateTime.parse(lateTime))
+                .setWarehouseId(Integer.valueOf(warehouseCode))
+                .setMsg(StrUtil.format("【{}-2B出货要求】：交易单号：{} 客商名称：{} 客商编码：{} 发货要求：{}",
+                        brand, orderId, customerInfo.getDisplayName(),
+                        customerInfo.getDistributorNum(), ""));
+    }
+
+    // 消息内容构建方法
+    private static String buildFuLaiMessage(boolean validityPeriod, boolean checkSample,
+                                            boolean checkBoxSize, String requirement) {
+
+        StringBuilder msg = new StringBuilder();
+        if (!validityPeriod) {
+            msg.append("按效期先后发货");
+        }
+        msg.append(checkSample ? "|纯小样" : "|需要扫溯源码出库");
+        msg.append(checkBoxSize ? "|按箱规发货" : "").append("|").append(requirement);
+        return msg.toString();
     }
 
     /**
@@ -140,6 +195,24 @@ public class SalesOrderAuditTaskEndListener {
             throw new BusinessException("销售单已发货，无法重复操作");
         }
         return ordersInfo;
+    }
+
+    private String handleBusinessError(String processId, BusinessException e) {
+        log.error("[流程 {}] 业务异常: {}", processId, e.getMessage(), e);
+        throw e;
+    }
+
+    private String handleSystemError(String processId, Exception e) {
+        String errorMsg = StrUtil.format("系统异常: {}", e.getMessage());
+        log.error("[流程 {}] 处理异常", processId, e);
+        throw new BusinessException(errorMsg);
+    }
+
+    private void logErrorIfNeeded(String processId, String errorMsg) {
+        if (StrUtil.isNotBlank(errorMsg)) {
+            BizErrorLogService service = SpringUtil.getObject(BizErrorLogService.class);
+            service.createBizErrorLog(LOG_MODULE, LOG_TYPE, processId, null, errorMsg);
+        }
     }
 }
 
